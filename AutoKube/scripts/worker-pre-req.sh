6890fd5cadb3295066034d54801cdacd31e45dcd
Salah-Eddine Gassim <s.gassim@gmail.com>
2021-02-24 18:23:09 +0100

    correct the kubelet node ip

--- AutoKube/scripts/worker-pre-req.sh
+++ AutoKube/scripts/worker-pre-req.sh
@@ -44,3 +44,8 @@ sudo apt-mark hold kubelet kubeadm kubectl
 ########### join the cluster
 
 sh /vagrant_data/join.sh
+
+########### correct internal ip for the kubelet
+ETH0=$(ip -f inet addr show eth1 | grep -Po 'inet \K[\d.]+')
+sudo sed -i "s/^ExecStart=\/usr\/bin\/kubelet.*/&  --node-ip $ETH0/"  /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+sudo systemctl daemon-reload && sudo systemctl restart kubelet
