2bcd3d1da6f0fb2c38c0fd432a23f914f254b0b1
Salah-Eddine Gassim <s.gassim@gmail.com>
2021-02-24 18:24:09 +0100

    correct kubelet node ip

--- AutoKube/scripts/master-pre-req.sh
+++ AutoKube/scripts/master-pre-req.sh
@@ -45,6 +45,11 @@ sudo apt-mark hold kubelet kubeadm kubectl
 ########### init cluster
 sudo kubeadm init  --apiserver-advertise-address=$API_IP --pod-network-cidr=$POD_CIDR
 
+########### correct internal ip for the kubelet
+ETH0=$(ip -f inet addr show eth1 | grep -Po 'inet \K[\d.]+')
+sudo sed -i "s/^ExecStart=\/usr\/bin\/kubelet.*/&  --node-ip $ETH0/"  /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+sudo systemctl daemon-reload && sudo systemctl restart kubelet
+
 ### configure kubectl
 
 mkdir -p $HOME/.kube
